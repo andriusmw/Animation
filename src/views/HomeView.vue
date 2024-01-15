fcc2c5ee2573988b8843c4f893ca02c00faeda96
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-15 15:54:37 +0100

    v-model v-for keypress

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,20 @@
 <script setup>
   import {ref} from "vue"
 
-  const showGreet = ref(true)
+  // const showGreet = ref(true)
+  const names = ref(["Selena Damo", "Patrick"])
+  const nameInput = ref("")
+
+  const addInvitee = () => {
+    names.value.unshift(nameInput.value);
+    nameInput.value = ""
+  }
 </script>
 
 <template>
+
   <main>
+    <!--
     <div class="container">
       <Transition name="fade">
         <h1 v-if="showGreet">Hello World</h1>
@@ -13,11 +22,26 @@
     </Transition>
     </div>
   
-   <button @click="showGreet= !showGreet">Toggle</button>
-  </main>
+   <button @click="showGreet= !showGreet">Toggle</button> -->
+ <div class="container">
+  <input 
+    type="text" 
+    placeholder="Add person to invite..."
+    v-model="nameInput"
+    @keypress.enter="addInvitee"
+    >
+  <ul>
+    <li v-for="name in names" :key="name">{{name}}</li>
+
+
+  </ul>
+ </div>
+ 
+  </main> 
 </template>
 
 <style scoped>
+/*
   .fade-enter-from {
   opacity: 0;
   }
@@ -54,5 +78,39 @@ h1 {
 button {
 
 margin-top: 50px;
+}
+
+*/
+
+.container{
+  max-width: 300px;
+  margin: 0 auto;
+}
+
+.container input {
+  width: 100%;
+  border-radius: 5px;
+  border:  1px solid rgba(128,128,128,0.13);
+  padding: 10px;
+  margin-bottom: 20px;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.12);
+}
+
+ul {
+  display: inline;
+  list-style: none;
+  margin: 0px;
+  padding: 0px
+}
+
+li {
+  list-style: none;
+  width: 300px;
+  border-radius: 5px;
+  padding: 5px 10px;
+  margin-top: 10px;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.12);
+  text-align: center;
+  cursor: pointer;
 }
 </style>
