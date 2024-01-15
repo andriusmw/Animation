7ceb2ae45d0d4f6354003852cffd9f492f47482d
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-15 15:21:07 +0100

    fade animation 2 leave

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,4 +27,20 @@
   transition: all 1s ease;
 }
 
+  .fade-leave-from {
+  opacity: 1;
+  }
+
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-leave-active {
+  transition: all 1s ease;
+}
+
+
+
+
+
 </style>
