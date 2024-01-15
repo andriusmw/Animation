0d1d08613d938f86d9e9a154039008f80eec59c7
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-15 15:12:06 +0100

    first color transition

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+  import {ref} from "vue"
+
+  const showText = ref(false)
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Transition name="color">
+        <h1 v-if="showText">Hello World</h1>
+    </Transition>
+  
+   <button @click="showText= !showText">Toggle</button>
   </main>
 </template>
+
+<style scoped>
+  .color-enter-from {
+    background-color: yellow;
+  }
+
+.color-enter-to {
+  background-color: blue;
+}
+
+.color-enter-active {
+  transition: all 5s ease;
+}
+
+</style>
